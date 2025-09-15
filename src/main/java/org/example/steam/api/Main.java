c3e3fb341b903cf34e0169b4c54dcf9cab052f8d
Anton Tsarenko <itantontsarenko@gmail.com>
2025-09-15 15:29:37 +0300

    feat: Added Zipper for streams

--- src/main/java/org/example/steam/api/Main.java
+++ src/main/java/org/example/steam/api/Main.java
@@ -6,12 +6,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> names = List.of("Alice", "Bob", "Charlie", "David", "Eve", "Frank");
-        String[] numberStrings = {"3, 5, 1", "4, 2", "8, 6, 7"};
 
         System.out.println();
         OddNumberPrinter.printOddNames(names);
         System.out.println("--------");
         DescendingSorter.printSortedDescending(names);
+
+        String[] numberStrings = {"3, 5, 1", "4, 2", "8, 6, 7"};
+
         System.out.println("--------");
         NumbersPrinter.printNumbers(numberStrings);
         System.out.println("--------");
@@ -19,5 +21,9 @@ public class Main {
                 .generateRandomInfinitySteam(25214903917L, 11, 281474976710656L)
                 .limit(10)
                 .toArray()));
+        System.out.println("--------");
+        System.out.println(StreamsZipper.zip(
+                names.stream(),
+                Arrays.stream(numberStrings)).toList());
     }
 }
